ee1eb49e33aad70ab60058228e20c5d6a6f11bab
Pavel Gushchin <pavel_gushchin@mail.ru>
2021-07-09 02:38:08 +0700

    Completed the skeleton for bot

--- main.py
+++ main.py
@@ -1,22 +1,85 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from secret_token import TOKEN
+#!/usr/bin/env python3
+
 import logging
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+from telegram import (
+    Update,
+)
+from telegram.ext import (
+    Updater,
+    CallbackContext,
+    Filters,
+    CommandHandler,
+    MessageHandler,
+)
 
-def start(update, context):
-    context.bot.send_message(update.effective_chat.id, 'Hello')
+from secret_token import TOKEN
 
-def echo(update, context):
-    context.bot.send_message(update.effective_chat.id, update.message.text)
+# Configure logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
 
-def caps(update, context):
-    text_caps = ' '.join(context.args).upper()
-    context.bot.send_message(update.effective_chat.id, text_caps)
 
-updater = Updater(TOKEN)
-updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), echo))
-updater.dispatcher.add_handler(CommandHandler('caps', caps))
+def help(update: Update, context: CallbackContext) -> None:
+    """Function for /help command"""
 
-updater.start_polling()
+    # Send help message
+    update.message.reply_text(
+        "What can I do:\n\n  /start - say 'hello'\n\n  /movie - suggest an interesting movie\n\n  /series - suggest an interesting TV series\n\n  /help - show help message"
+    )
+
+
+def start(update: Update, context: CallbackContext) -> None:
+    """Function for /start command"""
+
+    # Send "Hello" sticker
+    update.message.reply_sticker("CAACAgIAAxkBAAIBdWDmwUbnbcGuw7gMeJ_JF7QQq0uaAAKZDAACP1QBSs-TDHlrwSKUIAQ")
+
+    # Show help message
+    help(update, context)
+
+
+def movie(update: Update, context: CallbackContext) -> None:
+    """Function for /movie command. It suggests an interesting movie for user"""
+    pass
+
+
+def series(update: Update, context: CallbackContext) -> None:
+    """Function for /series command. It suggests an interesting TV series for user"""
+    pass
+
+
+def message_from_user(update: Update, context: CallbackContext) -> None:
+    """Function for replying to user, which sent any message to this bot"""
+
+    # Send "I don't understand you" message
+    update.message.reply_text("I don't understand you!")
+
+    # Send "Cry" sticker
+    update.message.reply_sticker("CAACAgIAAxkBAAIBj2Dmz9_eF73lcpR-3VVjXf0kytVdAAJhAAN4qOYPf6tqRsGZTcYgBA")
+
+    # Show help message
+    help(update, context)
+
+
+def main() -> None:
+    # Create the Updater and pass it your bot's token
+    updater = Updater(TOKEN)
+
+    # Register handlers
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CommandHandler('movie', movie))
+    dp.add_handler(CommandHandler('series', movie))
+    dp.add_handler(CommandHandler('help', help))
+    dp.add_handler(MessageHandler(Filters.all, message_from_user))
+
+    # Run the bot
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
